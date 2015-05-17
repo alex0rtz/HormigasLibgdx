31c59e3cc80ceebcbe6c905ef315aeec78463650
Alex <smx2ortiz@gmail.com>
2015-05-17 21:59:16 +0200

    Añadido la opción de cerrar la aplicación con el botón Esc y intrducirle pos Y y pos X al poligono que contienen las Plantas

--- core/src/com/test/hormigas/PantallaHormiga.java
+++ core/src/com/test/hormigas/PantallaHormiga.java
@@ -16,7 +16,7 @@ import java.util.Vector;
 
 public class PantallaHormiga implements Screen {
 
-    private static final int HORMIGAS_POR_CLIC = 100;
+    private static final int HORMIGAS_POR_CLIC = 1;
     private Stage stage;
     private FillViewport viewport;
 
@@ -56,6 +56,8 @@ public class PantallaHormiga implements Screen {
 
         act(delta);
         draw();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
     }
 
 
@@ -109,6 +111,7 @@ public class PantallaHormiga implements Screen {
             Random ran = new Random();
             actores.insertElementAt(new Planta(ran.nextFloat() * (Assets.screenWidth - Planta.TAMANO), ran.nextFloat() * (Assets.screenHeight - Planta.TAMANO)), posActores);
             stage.addActor(actores.get(posActores));
+            actores.get(posActores).getPolygon().setPosition(actores.get(posActores).getX(), actores.get(posActores).getY());
             posActores++;
         }
     }
@@ -123,35 +126,26 @@ public class PantallaHormiga implements Screen {
                     choque(act1, act2);
                 }
             }
-
         }
     }
 
     public void choque(final MyActor actor1, final MyActor actor2) {
-
         if (actor1 instanceof Hormiga && actor2 instanceof Hormiga)
             choqueEntreHormigas((Hormiga) actor1, (Hormiga) actor2);
         else if (actor1 instanceof Hormiga && actor2 instanceof Planta)
             choqueHormigaPlanta((Hormiga) actor1, (Planta) actor2);
         else if (actor1 instanceof Planta && actor2 instanceof Hormiga)
             choqueHormigaPlanta((Hormiga) actor2, (Planta) actor1);
-
     }
 
     private void choqueHormigaPlanta(Hormiga hormiga, Planta planta) {
-
-
         if (!hormiga.isChocada()) {
             hormiga.setChocada(true);
             hormiga.clearActions();
-            hormiga.mover();
         }
-
     }
 
     private void choqueEntreHormigas(final Hormiga h1, final Hormiga h2) {
-
-
         if (!h1.isChocada()) {
             h1.setChocada(true);
             h1.clearActions();
